b9c489bee0443a9096bf598d0512f0c186481c41
Kuzma Kukushkin <basskuzz@yandex.ru>
2023-11-16 14:50:08 +0300

    second commit

---
+++ hw_task_2/task2_kukushkin.py
@@ -0,0 +1,18 @@
+# task 1
+
+input_string = 'Python php function spam'
+stop_words = ['Python', 'php', 'go', 'C']
+
+words = input_string.split()
+print(stop_words)
+#result_words = list(filter(lambda x: x.lower() not in stop_words, words))
+#result_string = ' '.join(result_words)
+#print(words)
+
+
+#def remove_stop_words(input_string, stop_words):
+
+#    return result_string
+
+#print(remove_stop_words(input_string, stop_words))
+print('rrr')
